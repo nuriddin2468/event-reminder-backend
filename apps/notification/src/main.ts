59fe0f948ddce4bfa4e6ed7f624fa5603e1cf053
nuriddinyuldashev <nuriddin.yuldashev@axtechnology.uz>
2024-11-20 17:10:54 +0500

    init microservice

--- apps/notification/src/main.ts
+++ apps/notification/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { NotificationModule } from './notification.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(NotificationModule);
-  await app.listen(process.env.port ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    NotificationModule,
+    {
+      transport: Transport.REDIS,
+      options: {
+        port: 3001,
+      },
+    },
+  );
+  await app.listen();
 }
 bootstrap();
--- apps/notification/src/notification.service.ts
+++ apps/notification/src/notification.service.ts
@@ -1,8 +1,4 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class NotificationService {
-  getHello(): string {
-    return 'Hello World!';
-  }
-}
+export class NotificationService {}
