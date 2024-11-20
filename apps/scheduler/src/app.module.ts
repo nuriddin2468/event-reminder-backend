686293bbe8518228e5ce41da7e131fd62ed795aa
nuriddinyuldashev <nuriddin.yuldashev@axtechnology.uz>
2024-11-20 18:38:03 +0500

    auth, event, location modules init. ConfigModule, Graphql:Code-first, Typeorm init

--- apps/scheduler/src/app.module.ts
+++ apps/scheduler/src/app.module.ts
@@ -1,9 +1,39 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { LocationModule } from './location/location.module';
+import { EventModule } from './event/event.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        connectString: config.get('DATABASE_URL'),
+        synchronize: true,
+        autoLoadEntities: true,
+      }),
+    }),
+    AuthModule,
+    EventModule,
+    LocationModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
